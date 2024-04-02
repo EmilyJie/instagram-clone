c565a0ffd6238366facf99c6f3e484ad292fa4d3
Yi-Jhen Jie <emily6331@gmail.com>
2024-04-02 14:43:46 +0800

    Enhanced Sidebar with Chakra UI and React Router - Replaced a basic `div` with a Chakra UI `Box` component for the sidebar, improving styling and responsiveness. - Introduced a `Flex` container to manage the layout of sidebar contents more effectively, utilizing Chakra UI's flexbox utilities. - Integrated React Router with Chakra UI's `Link` component, enabling navigation within the application without page reloads. - Utilized the `InstagramLogo` component within the sidebar for brand representation, assuming it's a custom component that renders an Instagram-like logo.

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,3 +1,28 @@
+import { Box, Flex, Link } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+
 export default function Sidebar() {
-    return <div>Sidebar</div>;
+    return (
+        <Box
+            height={"100vh"}
+            borderRight={"1px solid"}
+            borderColor={"whiteAlpha.300"}
+            py={8}
+            position={"sticky"}
+            top={0}
+            left={0}
+            px={{ base: 2, md: 4 }}
+        >
+            <Flex direction={"column"} gap={10} w={"full"} height={"full"}>
+                <Link
+                    to={"/"}
+                    as={RouterLink}
+                    pl={2}
+                    display={{ base: "none", md: "block" }}
+                >
+                    <InstagramLogo />
+                </Link>
+            </Flex>
+        </Box>
+    );
 }
