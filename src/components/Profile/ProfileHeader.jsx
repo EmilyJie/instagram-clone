9987967a427a86d2870433203615feb3b54d1efb
Yi-Jhen Jie <emily6331@gmail.com>
2024-05-28 21:32:05 +0800

    Feat: Add follow and unfollow functions

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -10,15 +10,20 @@ import {
 import useUserProfileStore from "../../store/userProfileStore";
 import useAuthStore from "../../store/authStore";
 import EditProfile from "./EditProfile";
+import useFollowUser from "../../hooks/useFollowUser";
 
 export default function ProfileHeader() {
     const { userProfile } = useUserProfileStore();
     const authUser = useAuthStore((state) => state.user);
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { isFollowing, isUpdating, handleFollowUser } = useFollowUser(
+        userProfile?.uid
+    );
+
     const visitingOwnProfileAndAuth =
         authUser && authUser.username === userProfile?.username;
     const visitingAnotherProfileAndAuth =
         authUser && authUser.username !== userProfile?.username;
-    const { isOpen, onOpen, onClose } = useDisclosure();
 
     return (
         <Flex
@@ -79,8 +84,10 @@ export default function ProfileHeader() {
                                 color={"white"}
                                 _hover={{ bg: "blue.600" }}
                                 size={{ base: "xs", md: "sm" }}
+                                isLoading={isUpdating}
+                                onClick={handleFollowUser}
                             >
-                                Follow
+                                {isFollowing ? "Unfollow" : "Follow"}
                             </Button>
                         </Flex>
                     )}
