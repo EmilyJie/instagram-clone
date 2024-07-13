728be22dcea818fa95a99f788c553e89a2437a3c
Yi-Jhen Jie <emily6331@gmail.com>
2024-07-13 16:14:53 +0800

    feat: Add useGetUserPosts hook for fetching user posts

--- src/store/postStore.js
+++ src/store/postStore.js
@@ -3,6 +3,7 @@ import { create } from "zustand";
 const usePostStore = create((set) => ({
     post: [],
     createPost: (post) => set((state) => ({ post: [post, ...state.post] })),
+    setPosts: (posts) => set({ post: posts }),
 }));
 
 export default usePostStore;
