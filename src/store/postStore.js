06562d696dd2ad19e70a6ad2eec6229af982cd7a
Yi-Jhen Jie <emily6331@gmail.com>
2024-07-21 18:44:00 +0800

    Optimize delete post functionality

--- src/store/postStore.js
+++ src/store/postStore.js
@@ -4,6 +4,8 @@ const usePostStore = create((set) => ({
     post: [],
     createPost: (post) => set((state) => ({ post: [post, ...state.post] })),
     setPosts: (posts) => set({ posts }),
+    deletePost: (id) =>
+        set((state) => ({ post: state.post.filter((post) => post.id !== id) })),
 }));
 
 export default usePostStore;
